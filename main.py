ed3d9d515d6c2bfd238dc55b84fbe4df4b1b66ea
Hopyoprop <yeo_jordan@yahoo.com.sg>
2019-01-26 16:50:03 +0800

    Adjusted Main.py for Dict User input

--- main.py
+++ main.py
@@ -130,11 +130,20 @@ if __name__ == "__main__":
 
     # GUI to obtain user's profile and what is his profile
     # TO DO
-    userlist = []   # TEMPORARY PLACEHOLDER LIST. USERLIST WILL BE THE USER GUI INPUTTED VALUES
+
+    ##########################################################################################################
+    # Function 2
+    userlist = {}   # TEMPORARY PLACEHOLDER DICT. USERLIST WILL BE THE USER GUI INPUTTED VALUES
     acceptedcountry = {} # TEMPORARY LOCATION. TO BE MOVED. WILL STORE THE SCALED DOWN MAINDICT WITH USERS OF ACCEPTABLE
     # COUNTRY
     # First round of Processing for Acceptable Countries, returns a dict with accepted users
     acceptedcountry = processUser(userlist, maindict)
+
+    ##########################################################################################################
+    #Function 3
+    #TO DO
+
+    ##########################################################################################################
     # once processing done, take dictionary/list of user input and compare with each dictionary of processed profile
     # DTF_Match()
 
