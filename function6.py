a493b2ac180df03bd4ab5305673f9af8d355f5a5
Vexasoe <vexasoe@gmail.com>
2019-02-12 11:21:54 +0800

    Function 6 working, just need top 3 name

--- function6.py
+++ function6.py
@@ -2,14 +2,17 @@ import csv
 import sqlite3
 
 
-def exportdata(name):
-    conn = sqlite3.connect('userprofiles.db')
+def exportdata(name1,name2,name3):      # get top 3 best matched name
+    conn = sqlite3.connect('userprofiles.db')        # connect to database
     # create cursor object, and assign it to variable called c
-    c = conn.cursor()
-    c.execute("SELECT %s FROM userprofs" % name)
-    with open("top3.csv", "w", newline='') as csv_file:
+    c = conn.cursor() #pinpoint records in a database. 
+    c.execute("SELECT Name,Gender,Country,Age,Likes,Dislikes,Books FROM profiles WHERE Name IN (?,?,?)",(name1,name2,name3))      # execute the sql to get top 3 best match data
+    with open("top3.csv", "w") as csv_file: #For writing. Overwrites the file if the file exists. Otherwise, creates a new file for writing, and name for top3.csv
         csv_writer = csv.writer(csv_file)
-        csv_writer.writerow([i[0] for i in cursor.description])  # write headers
-        csv_writer.writerows(cursor)
+        csv_writer.writerow([i[0] for i in c.description]) # write headers
+        csv_writer.writerows(c)
     conn.commit()
     conn.close()
+
+
+#exportdata('Teresa','Carol','Kevin') # trying if function works
